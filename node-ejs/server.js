8bba058edc41dd7f4b8e324c14d6920945b91868
Celso Lopes <celsolopesjr@hotmail.com>
2022-07-21 21:40:50 -0300

    [Node +EJS] Usando forEach

--- node-ejs/server.js
+++ node-ejs/server.js
@@ -6,7 +6,27 @@ app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
-    res.render("pages/index");
+    const items = [
+        { 
+            title: "D",
+            message: "Algo com D",
+        },
+        { 
+            title: "E",
+            message: "Algo com E",
+        },
+        { 
+            title: "M",
+            message: "Algo com M",
+        },
+        { 
+            title: "A",
+            message: "Algo com A",
+        }
+    ];
+    res.render("pages/index", {
+        qualitys: items,
+    });
 })
 
 app.get("/sobre", function(req, res) {
